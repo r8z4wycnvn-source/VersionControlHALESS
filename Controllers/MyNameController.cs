2de8dd8a043545595ea12ceb9e0f7bc88eb5d366
Jimeir Hales <r8z4wycnvn@privaterelay.appleid.com>
2025-10-31 20:49:26 -0400

    Added MyNameController returning JSON

---
+++ Controllers/MyNameController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VersionControlHALES.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyNameController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMyName()
+        {
+            var data = new { Name = "Jimeir Hales" };
+            return new JsonResult(data);
+        }
+    }
+}
